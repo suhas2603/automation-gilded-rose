bf638c6c43228c347e9d3cec74b824a96ca78a2c
suhas2603 <36885949+suhas2603@users.noreply.github.com>
2018-03-15 11:39:39 +0100

    Updated the file for the missing tests
    Added the missing tests and now all the scenarios and test steps run successfully.

--- lib/google_shopping.rb
+++ lib/google_shopping.rb
@@ -4,28 +4,54 @@ class GoogleShopping
   include Capybara::DSL
 
   def verify_search(string)
-    within 'div.sh-pr__product-results' do
-      result = first'div.psli h3'
-      if result.text.slice(string).nil?
+    elements = all('div.pslline')
+    case "string"
+      when 'marmite'
+        elements.each do |element|
+          unless element.text =~ /Marmite/
+            fail
+          end
+        end
+    end
+  end
+
+  def verify_books
+    find(:xpath, '//*[@id="lb"]/div/a[3]').click
+    sleep(1)
+  end
+
+  def verify_seller_ocado(string)
+    elements = all('div.pslline')
+    case "string"
+      when 'ocado'
+        elements.each do |element|
+          unless element.text =~ /Ocado/
+            fail
+          end
+        end
+    end
+  end
+
+  def verify_seller_eatbig(string)
+    elements = all('div.pslline')
+    case "string"
+      when 'eatbig'
+        elements.each do |element|
+          unless element.text =~ /Eatbig/
+            fail
+          end
+        end
+    end
+  end
+
+  def verify_range()
+    elements = all('span.price')
+    prices = elements.map { |e| e.text.sub('£', '').to_i }
+    prices.each do |price|
+      unless price >=20 && price <= 30
         fail
       end
     end
   end
 
-  def click_on_books_category
-    find('span[title=Books]').click
-    sleep(2)
-  end
-
-  def verify_shopping(string)
-    elements = all('div.pslline')
-    case "string"
-    when 'ocado'
-      elements.each do |element|
-        unless element.text =~ /Ocado/
-          fail
-        end
-      end
-    end
-  end
 end
